e5b018aa9630c7fbebc343fa34d70f386523b60f
Lindberg <grantlindberg@grant-lindbergs-imac.local>
2018-02-20 13:04:19 -0800

    Create BoardView subclass and outlet

---
+++ BoardView.swift
@@ -0,0 +1,21 @@
+//
+//  BoardView.swift
+//  FifteenPuzzle
+//
+//  Created by Lindberg on 2/20/18.
+//  Copyright © 2018 Lindberg. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
